82666892e0da07491b95263d558dad24984f5c5a
Arthur Chauvel <arthurchauveliut@gmail.com>
2024-11-20 19:37:35 +0100

    exercice 5 s102

---
+++ Séance1/exo5.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+time_t debut = time(NULL);
+for (int i=0 ; i<100000 ; i++){
+    for (int j=0 ; j<100000 ; j++){
+    }
+ }
+ time_t fin = time(NULL);
+ printf("durée = %.3f secondes\n", difftime(fin, debut));
